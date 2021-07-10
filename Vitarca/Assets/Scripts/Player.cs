1af6b5c13574fce319f6c0c8662a48c28ceb5696
Joseph <joejmitchell@hotmail.co.uk>
2021-07-10 21:36:28 +0100

    Updated player movement

--- Vitarca/Assets/Scripts/Player.cs
+++ Vitarca/Assets/Scripts/Player.cs
@@ -2,10 +2,39 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] float runSpeedMult = 0.01f;
+    private enum State
+    {
+        Walking,
+        Rolling
+    }
+
+    [SerializeField] private float runSpeedMult;
+    [SerializeField] private Vector2 maxVelocity;
+    private State state = State.Walking;
+
+    private Rigidbody2D rb;
+
+    void horizontalMove()
+    {
+        if (state == State.Walking)
+        {
+            rb.AddForce(new Vector3(Input.GetAxis("Horizontal") * runSpeedMult * Time.deltaTime, 0, 0), ForceMode2D.Impulse);
+        }
+        else
+        {
+            //Rollin stuff later
+        }
+    }
+
+    private void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
 
     void Update()
     {
-        gameObject.transform.position += new Vector3(Input.GetAxis("Horizontal") * runSpeedMult, 0, 0);
+        horizontalMove();
+
+        rb.velocity = new Vector2(Mathf.Min(Mathf.Abs(rb.velocity.x), maxVelocity.x) * Mathf.Sign(rb.velocity.x), Mathf.Min(Mathf.Abs(rb.velocity.y), maxVelocity.y) * Mathf.Sign(rb.velocity.y));
     }
 }
