b8663acf2a3a67d6c97086238992629ec22c8b71
Joseph <joejmitchell@hotmail.co.uk>
2021-07-17 18:26:48 +0100

    Updated player movement and input manager
    Added separate physics materials for player. Materials switch based on current movement so that player can move with little friction but will slow down quickly with no input.
    Updated horizontal input, axis value now snaps between -1, 0 and 1 without smoothing.

--- Vitarca/Assets/Scripts/Player.cs
+++ Vitarca/Assets/Scripts/Player.cs
@@ -8,6 +8,7 @@ public class Player : MonoBehaviour
         Rolling
     }
 
+    [SerializeField] private PhysicsMaterial2D physMatHF, physMatLF; //*High and low friction physics materials*//
     [SerializeField] private float runSpeedMult;
     [SerializeField] private Vector2 maxVelocity;
     private State state = State.Walking;
@@ -18,6 +19,17 @@ public class Player : MonoBehaviour
     {
         if (state == State.Walking)
         {
+            if (Mathf.Sign(Input.GetAxis("Horizontal")) != Mathf.Sign(rb.velocity.x) || Input.GetAxis("Horizontal") == 0)
+            {
+                rb.sharedMaterial = physMatHF;
+            }
+            else if (Mathf.Sign(Input.GetAxis("Horizontal")) == Mathf.Sign(rb.velocity.x) || rb.velocity.x == 0)
+            {
+                rb.sharedMaterial = physMatLF;
+            }
+            
+            Debug.Log("" + Input.GetAxis("Horizontal") + " " + rb.velocity.x);
+
             rb.AddForce(new Vector3(Input.GetAxis("Horizontal") * runSpeedMult * Time.deltaTime, 0, 0), ForceMode2D.Impulse);
         }
         else
